5cecd504988fff8406fecfb3ae2c9f928e51dc32
mi-skam <maksim.codes@mailbox.org>
2021-06-16 14:47:20 +0200

    adding git config

--- home-linux.nix
+++ home-linux.nix
@@ -1,8 +1,18 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
-    home.packages = with pkgs; [
-        jq
-        tree
-        nixUnstable
-    ];
+  home.packages = with pkgs; [
+    jq
+    tree
+    htop
+
+    nixUnstable
+    nixpkgs-fmt # nix code formatter
+    rnix-lsp # nix language server
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "mi-skam";
+    userEmail = "maksim.codes@mailbox.org";
+  };
 }
