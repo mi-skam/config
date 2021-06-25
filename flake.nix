0494b38580205da37a68a8590cc1f59d743c3f33
mi-skam <maksim.codes@mailbox.org>
2021-06-25 15:11:51 +0200

    relaying openssh-agent to wsl

--- flake.nix
+++ flake.nix
@@ -2,19 +2,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    npiperelay = {
+      url = "path:./npiperelay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:rycee/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { home-manager, nixpkgs, ... }:
+  outputs = { home-manager, nixpkgs, npiperelay, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { inherit system; overlays = [ npiperelay.overlay ]; };
       up = pkgs.writeScriptBin "up" ''
         #!${pkgs.stdenv.shell}
-	nix build .#homeManagerConfigurations.linux.activationPackage && ./result/activate
+        nix build .#homeManagerConfigurations.linux.activationPackage && ./result/activate
       '';
     in
       {
@@ -24,12 +29,13 @@
             system = system;
             homeDirectory = "/home/plumps";
             username = "plumps";
+            pkgs = pkgs;
           };
         };
         devShell.${system} = pkgs.mkShell {
-	 description = "hm-shell";
-	 nativeBuildInputs = [ pkgs.bashInteractive ];
-	 buildInputs = [ up ];
-	};
+          description = "hm-shell";
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = [ up ];
+        };
       };
 }
--- home-linux.nix
+++ home-linux.nix
@@ -18,15 +18,25 @@
     nixUnstable
     nixpkgs-fmt # nix code formatter
     rnix-lsp # nix language server
+
   ];
 
-  programs= {
+  programs = {
     autojump = {
       enable = true;
       enableBashIntegration = true;
     };
     bash = {
       enable = true;
+      bashrcExtra = ''
+        export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+
+        ss -a | grep -q $SSH_AUTH_SOCK
+        if [ $? -ne 0 ]; then
+            rm -f $SSH_AUTH_SOCK
+            (setsid ${pkgs.socat}/bin/socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"${pkgs.npiperelay}/bin/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+        fi       
+      '';
       profileExtra = ''
         . /home/plumps/.nix-profile/etc/profile.d/nix.sh
       '';
@@ -36,7 +46,7 @@
       enableBashIntegration = true;
       nix-direnv = {
         enable = true;
-	enableFlakes = true;
+        enableFlakes = true;
       };
     };
     emacs = {
